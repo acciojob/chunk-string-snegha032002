965860569390281242d8c36fb50c15f7f92994b5
Snegha R <sneghakumar0315@gmail.com>
2024-12-30 18:07:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function stringChop(str, size) {
   // your code here
+	if (!str) return [];
+  
+  const result = [];
+  
+
+  for (let i = 0; i < str.length; i += size) {
+    result.push(str.slice(i, i + size));
+  }
+  
+  return result;
 }
 
 // Do not change the code below
